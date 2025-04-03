6c31b5b9f2cbfe7a8b48ecfeb72d7dd59f6c86d8
Abdi <89213120+abdirashidexe@users.noreply.github.com>
2025-04-03 11:36:04 -0700

    2nd problem done

--- src/Practice.java
+++ src/Practice.java
@@ -18,8 +18,9 @@ public class Practice {
     return evens;
   }
 
-  // Time Complexity:
-  // Space Complexity: 
+  // n = matrix.length
+  // Time Complexity: O(n)
+  // Space Complexity: O(1)
   public static int sumDiagonal(int[][] matrix) {
     int sum = 0;
     for (int i = 0; i < matrix.length; i++) {
