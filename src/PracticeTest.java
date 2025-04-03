fa573d2702c7538c118f860947d8ebc22194e0cc
Jacob Woodbury <jacobswoodbury@gmail.com>
2025-04-03 12:27:20 -0700

    writing test for SpaceEff

--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -10,8 +10,26 @@ public static int mostCommonTimeEfficient() {
 
 }
 
-public static mostCommonSpaceEfficient() {
 
+@Test
+public static void mostCommonSpaceEfficient_mixedNums() {
+  int[] arr = {1,2,3,4,4,4};
+  int actual = Practice.mostCommonSpaceEfficient(arr);
+  assertEquals(4, actual);
+}
+
+@Test
+public static void mostCommonSpaceEfficient_tie(){
+  int[] arr = {1,1,2,3,4,4};
+  int actual = Practice.mostCommonSpaceEfficient(arr);
+  assertEquals(1, actual);
+}
+
+@Test
+public static void mostCommonSpaceEfficient_noDups(){
+  int[] arr = {1,2,3,4};
+  int actual = Practice.mostCommonSpaceEfficient(arr);
+  assertEquals(1, actual);
 }
   // Hints: They are static methods, so you will use the full Practice.mostCommonTimeEfficient for method calls
 }
