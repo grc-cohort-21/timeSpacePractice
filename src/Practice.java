fdf5646be4f464d4882f7eaedab915ea0980ec6a
Squizard <184549450+BradyLeg@users.noreply.github.com>
2025-04-03 11:40:52 -0700

    Fourth question

--- src/Practice.java
+++ src/Practice.java
@@ -39,8 +39,8 @@ public class Practice {
     return frequencies;
   }
 
-  // Time Complexity:
-  // Space Complexity:
+  // Time Complexity: O(n^2)
+  // Space Complexity: O(n)
   public static List<Integer> evensToSquare(int n) {
     List<Integer> evens = new ArrayList<>();
     for (int i = 0; i <= n * n; i += 2) {
