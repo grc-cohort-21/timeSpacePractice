2c516f1f7730bb1527a882ee1a0dc20981dda40e
Squizard <184549450+BradyLeg@users.noreply.github.com>
2025-04-03 11:56:10 -0700

    Started on tests

--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -1,10 +1,16 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-
 public class PracticeTest {
-  
-  // TODO: Implement tests for Practice.mostCommonTimeEfficient and Practice.mostCommonSpaceEfficient
 
-  // Hints: They are static methods, so you will use the full Practice.mostCommonTimeEfficient for method calls
+  // TODO: Implement tests for Practice.mostCommonTimeEfficient and
+  // Practice.mostCommonSpaceEfficient
+
+  @Test
+  void mostCommonTimeEfficient() {
+
+  }
+
+  // Hints: They are static methods, so you will use the full
+  // Practice.mostCommonTimeEfficient for method calls
 }
