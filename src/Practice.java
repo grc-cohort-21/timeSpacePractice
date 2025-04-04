9e399ca60b764db4c80cb20ad9d8b31d8c1d5b4c
mlarsen <174657206+mlarsen-source@users.noreply.github.com>
2025-04-03 19:19:16 -0700

    implemented mostCommonTimeEfficient method

--- src/Practice.java
+++ src/Practice.java
@@ -5,8 +5,9 @@ import java.util.Map;
 
 public class Practice {
 
-  // Time Complexity:
-  // Space Complexity: 
+  // Time Complexity: O(n)
+  // Space Complexity: O(n)
+  // n is the length of array
   public static List<Integer> findEvens(int[] array) {
     List<Integer> evens = new ArrayList<>();
     for (int num : array) {
@@ -17,8 +18,9 @@ public class Practice {
     return evens;
   }
 
-  // Time Complexity:
-  // Space Complexity: 
+  // Time Complexity: O(n)
+  // Space Complexity: O(1)
+  // n is the length of the matrix
   public static int sumDiagonal(int[][] matrix) {
     int sum = 0;
     for (int i = 0; i < matrix.length; i++) {
@@ -28,8 +30,9 @@ public class Practice {
   }
   
 
-  // Time Complexity: 
-  // Space Complexity: 
+  // Time Complexity: O(n)
+  // Space Complexity: O(n))
+  // n is the length of array
   // Does the 'T' look confusing? Consider refreshing on generic methods
   // We'll revisit generics as a class later
   public static <T> Map<T, Integer> countFrequencies(T[] array) {
@@ -40,8 +43,9 @@ public class Practice {
     return frequencies;
   }
 
-  // Time Complexity: 
-  // Space Complexity: 
+  // Time Complexity: O(n^2)
+  // Space Complexity: O(n^2)
+  // n is the value of int n
   public static List<Integer> evensToSquare(int n) {
     List<Integer> evens = new ArrayList<>();
     for(int i = 0; i <= n*n; i+=2) {
@@ -59,16 +63,49 @@ public class Practice {
    * 
    * Once you finish, WRITE TESTS FOR IT in PracticeTest.java
    * 
-   * Time Complexity: 
-   * Space Complexity: 
+   * Time Complexity: O(n)
+   * Space Complexity: O(n)
+   * // n is the length of nums
    * 
    * @param nums An array of integers
    * @return the integer that shows up most commonly
    */
-  public static int mostCommonTimeEfficient(int[] nums) {
+  public static int mostCommonTimeEfficient(int[] nums) 
+  {
     // TODO: Complete this method with an implementation that runs
     // in O(n) time. n = nums.size()
-    return -1;
+
+    if(nums.length == 0)
+    {
+      return -1;
+    }
+
+    Map<Integer, Integer> intMap = new HashMap<>();
+    for(Integer num : nums)
+    {
+      if(intMap.containsKey(num))
+      {
+        intMap.put(num, intMap.get(num) + 1 );
+      }
+      else{
+        intMap.put(num, 1 );
+      }
+    }
+
+    int maxValue = 0;
+    int keyWithMaxValue = 0;
+
+    
+    for (Map.Entry<Integer, Integer> entry : intMap.entrySet()) 
+    {
+      if (entry.getValue() > maxValue) 
+      {
+          maxValue = entry.getValue();
+          keyWithMaxValue = entry.getKey();
+      }
+    }
+    return keyWithMaxValue;
+
   }
 
   /**
