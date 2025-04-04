b24e216a8dc50989b3a6eb4ae0594340c5f7dafe
mlarsen <174657206+mlarsen-source@users.noreply.github.com>
2025-04-03 19:29:24 -0700

    implemented tests for mostCommonTimeEfficient method

--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.Set;
 
 
 public class PracticeTest {
@@ -7,4 +8,65 @@ public class PracticeTest {
   // TODO: Implement tests for Practice.mostCommonTimeEfficient and Practice.mostCommonSpaceEfficient
 
   // Hints: They are static methods, so you will use the full Practice.mostCommonTimeEfficient for method calls
+
+// Tests for mostCommonTimeEfficient method //
+
+
+@Test
+void mostCommonTimeEfficient_SingleElement() 
+{
+  // arrange
+  int[] testArray = {100};
+  // act
+  int actual = Practice.mostCommonTimeEfficient(testArray);
+  //assert
+  assertEquals(100, actual);
+}
+
+
+@Test
+void mostCommonTimeEfficient_OneMostCommonValue() 
+{
+  // arrange
+  int[] testArray = {1, 2, 3, 1};
+  // act
+  int actual = Practice.mostCommonTimeEfficient(testArray);
+  //assert
+  assertEquals(1, actual);
+}
+
+@Test
+void mostCommonTimeEfficient_MultipleMostCommonValues() 
+{
+  // arrange
+  int[] testArray = {1, 2, 3, 1, 2, 2, 3, 3, 1};
+  // act
+  int actual = Practice.mostCommonTimeEfficient(testArray);
+  //assert
+  assertEquals(1, actual);
+}
+
+@Test
+void mostCommonTimeEfficient_EmptyArray() 
+{
+  // arrange
+  int[] testArray = {};
+  // act
+  int actual = Practice.mostCommonTimeEfficient(testArray);
+  //assert
+  assertEquals(-1, actual);
+}
+
+@Test
+void mostCommonTimeEfficient_NegativeElements() 
+{
+  // arrange
+  int[] testArray = {-1, 1, -2, 3, -1, 2, -3};
+  // act
+  int actual = Practice.mostCommonTimeEfficient(testArray);
+  //assert
+  assertEquals(-1, actual);
+}
+
+
 }
