e5c49a01733bcce4a0fbcc7f376daffee98f1ccc
VibeSyntax <138536097+beza102@users.noreply.github.com>
2025-04-06 22:14:49 -0700

    added a few test case

--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -7,4 +7,24 @@ public class PracticeTest {
   // TODO: Implement tests for Practice.mostCommonTimeEfficient and Practice.mostCommonSpaceEfficient
 
   // Hints: They are static methods, so you will use the full Practice.mostCommonTimeEfficient for method calls
+  @Test
+  public void testMostCommonTimeEff(){
+    //arrange
+    int[] input ={1, 2, 2, 2, 3, 3, 4, 5};
+    //act
+    int actual = Practice.mostCommonSpaceEfficient(input);
+    //assert
+    assertEquals(2, actual);
+
+  }
+
+  @Test 
+  public void testMostCommonTimeEff_tieBreaker(){
+    //arrange
+    int[]input ={4, 5, 4, 5};
+    //act
+    int actual =Practice.mostCommonSpaceEfficient(input);
+    //assert
+    assertEquals(4, actual);
+  }
 }
