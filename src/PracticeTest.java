38c031efc7ab90e889221788bab0213ba01155e8
EmilyM <155667566+EmilyMenken@user.noreply.github.com>
2025-04-07 17:18:08 -0700

    added test 'timeEfficientMostCommonNumWorking'

--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -7,4 +7,20 @@ public class PracticeTest {
   // TODO: Implement tests for Practice.mostCommonTimeEfficient and Practice.mostCommonSpaceEfficient
 
   // Hints: They are static methods, so you will use the full Practice.mostCommonTimeEfficient for method calls
-}
+
+  //tests for mostCommonTimeEfficient
+
+    @Test
+      public void timeEfficientMostCommonNumWorking() {
+      // Arrange
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 7, 7, 7, 7};
+    
+      // Act
+        int result = Practice.mostCommonTimeEfficient(numbers);
+    
+      // Assert
+        assertEquals(7, result); //expected res: 7
+    }
+
+
+}//end practiceTest
