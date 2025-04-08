bc22402fa87e3beacc60a88a3ce992f1cd93044e
EmilyM <155667566+EmilyMenken@user.noreply.github.com>
2025-04-07 17:50:31 -0700

    added test 'timeEfficientMostCommonNumOutOfOrder'

--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -20,19 +20,36 @@ public class PracticeTest {
     
       // Assert
         assertEquals(7, result); //expected res: 7, most common number
-    }
+    }//end working
 
     @Test
       public void timeEfficientMostCommonNumTie() {
       // Arrange
-       int[] nums = {1, 1, 2, 2, 3, 3};
+        int[] nums = {1, 1, 2, 2, 3, 3};
         
       // Act
         int result = Practice.mostCommonTimeEfficient(nums);
         
       // Assert
         assertEquals(1, result); //expected res: 1, the first most common number is never overwritten since its a tie
-    }
+    }//end tie
+
+    @Test
+      public void timeEfficientMostCommonNumOutOfOrder() {
+      // Arrange
+        int[] numerals = {1, 2, 3, 2, 3, 2, 3, 7, 3};
+        
+      // Act
+        int result = Practice.mostCommonTimeEfficient(numerals);
+        
+      // Assert
+        assertEquals(3, result); //expected res: 3, the most common number but out of order
+    }// end outOfOrder
+
+    
 
 
+
+
+    
 }//end practiceTest
