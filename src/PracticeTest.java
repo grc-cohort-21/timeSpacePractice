68cc76d31d5ddd4442c7c5e4af616280e061435a
unknown <174657090+Butenkite@users.noreply.github.com>
2025-04-12 11:24:40 -0700

    implementation of tests for space

--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -9,8 +9,11 @@ public class PracticeTest {
   // TODO: Implement tests for Practice.mostCommonTimeEfficient and Practice.mostCommonSpaceEfficient
 
   // Hints: They are static methods, so you will use the full Practice.mostCommonTimeEfficient for method calls
+
+
+  // time efficency
   @Test
-  void TimeEfficent_fiveHighestAppearsFiveTimes() {
+  void timeEfficent_fiveHighestAppearsFiveTimes() {
     // arrange
     int[] nums = {1, 9, 9, 4, 5, 5, 5, 5, 5};
     // act
@@ -20,11 +23,34 @@ public class PracticeTest {
   }
 
   @Test
-  void TimeEfficent_ThreeHighestAppearsThreeTimesMultipleKings() {
+  void timeEfficent_threeHighestAppearsThreeTimesMultipleKings() {
     // arrange
     int[] nums = {1, 3, 3, 3, 4, 8, 5, 5, 5};
     // act
     int actual = Practice.mostCommonTimeEfficient(nums);
+    //assert
+    assertEquals(3, actual);
+  }
+
+
+  // space effiency
+
+  @Test
+  void spaceEfficent_fiveHighestAppearsFiveTimes() {
+    // arrange
+    int[] nums = {1, 9, 9, 4, 5, 5, 5, 5, 5};
+    // act
+    int actual = Practice.mostCommonSpaceEfficient(nums);
+    //assert
+    assertEquals(5, actual);
+  }
+
+  @Test
+  void spaceEfficent_threeHighestAppearsThreeTimesMultipleKings() {
+    // arrange
+    int[] nums = {1, 3, 3, 3, 4, 8, 5, 5, 5};
+    // act
+    int actual = Practice.mostCommonSpaceEfficient(nums);
     //assert
     assertEquals(3, actual);
   }
